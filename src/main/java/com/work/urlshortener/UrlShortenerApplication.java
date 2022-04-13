c4af69f28dfa9fb821d9dc61f85d1b950d128d4a
Aydin UZUN <uzunx153@gmail.com>
2022-04-13 20:48:56 +0300

    created prod environment

--- src/main/java/com/work/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/work/urlshortener/UrlShortenerApplication.java
@@ -2,10 +2,13 @@ package com.work.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource("classpath:application-prod.properties")
+// @Profile("default")
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
