7e6f7899d99f8d7376a1d54846dcddc84597f87c
Aydin UZUN <uzunx153@gmail.com>
2022-04-13 22:30:09 +0300

    error

--- src/main/java/com/work/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/work/urlshortener/UrlShortenerApplication.java
@@ -2,12 +2,10 @@ package com.work.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
